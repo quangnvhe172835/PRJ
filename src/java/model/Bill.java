9b1e4b6d796199adf64df8de04ea8b6d26e63844
quangnvhe172835 <quangnvhe172835@fpt.edu.vn>
2024-12-07 16:19:59 +0700

    first commit

---
+++ src/java/model/Bill.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.sql.Date;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+
+public class Bill {
+
+    private int id;
+    private Date created_date;
+    private String status;
+    private Order order;
+    private User user;
+    
+}
